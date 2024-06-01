80d767800aa8bc21104b740e66c97c6314224b2e
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-05-31 20:15:07 -0500

    feat: new code structure

--- tests/test_simple_todos.py
+++ tests/test_simple_todos.py
@@ -3,28 +3,19 @@
 # Page: It is a single tab or window within a browser context. A browser context may have multiple pages. Typically, an individual test should interact with only one page.
 
 from playwright.sync_api import expect,Page
-from pages.todo_page import TodoPage
 
 def test_add_new_todo(page: Page,) -> None:
-
     # Given I am on the todo list page
     page.goto('/todomvc/#/')
     # When the user enters "Buy milk" into the add item input
-    page.locator('.new-todo').fill('Buy milk')
+    # page.locator('.new-todo').fill('Asistir a PyCon')
+    page.get_by_role('textbox', name="What needs to be done?").fill('Asistir a PyCon')
     # And the user press the "enter" keyboard
     page.keyboard.press('Enter')
     # Then "Buy milk" should be added to the list
-    expect(page.locator('[data-testid="todo-title"]')).to_have_text('Buy milk')
+    # https://playwright.dev/python/docs/test-assertions
+    expect(page.locator('[data-testid="todo-title"]')).to_have_text('Asistir a PyCon') 
 
-def test_add_new_todo_pom(page: Page) -> None:
-    todo_page = TodoPage(page)
-    todo_page.load()
-    todo_page.add_todo('Buy milk')
-    expect(todo_page.todo_item).to_have_text('Buy milk')
 
-def test_add_new_todo_pom_fixture(page: Page, load_todo) -> None:
-    todo = load_todo('todo1')
-    todo_page = TodoPage(page)
-    todo_page.load()
-    todo_page.add_todo(todo)
-    expect(todo_page.todo_item).to_have_text(todo)
+
+
